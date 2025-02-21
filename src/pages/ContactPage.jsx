f89ce499b46638c1ab84cde5a3e9c5c2ef66a0e1
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-02-21 16:11:34 -0300

    style: add destkop design body padding

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,7 +2,7 @@ import ContactForm from "../components/ContactForm";
 
 function ContactPage() {
   return (
-    <main className="grid min-h-screen place-items-center bg-green-200 px-4 py-8 font-karla">
+    <main className="grid min-h-screen place-items-center bg-green-200 px-4 py-8 font-karla sm:py-32">
       <ContactForm />
     </main>
   );
