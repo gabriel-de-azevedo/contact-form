d65656cc0f002d3619df0aeff79b458bf089d3f1
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-02-23 11:31:47 -0300

    fix: svg imports

--- src/atoms/FormToast.jsx
+++ src/atoms/FormToast.jsx
@@ -1,8 +1,10 @@
+import checkIcon from "../assets/icon-success-check.svg";
+
 function FormToast() {
   return (
     <div className="flex w-full flex-col gap-3 rounded-xl bg-green-900 p-7 text-white">
       <span className="flex gap-3">
-        <img src="src/assets/icon-success-check.svg" alt="Success Check Icon" />
+        <img src={checkIcon} alt="Success Check Icon" />
         <strong>Message Sent!</strong>
       </span>
       <p>{"Thanks for completing the form. We'll be in touch soon!"}</p>
